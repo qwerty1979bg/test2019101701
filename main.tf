00ab60b4dc8ad94ba485caf53e3ebb26a4a5fe45
qwerty1979bg <42143379+qwerty1979bg@users.noreply.github.com>
2019-10-17 14:37:21 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+variable "a1" { default="default" }
+variable "a2" { default="default" }
+variable "a3" { default="default" }
+variable "b1" { default="default" }
+variable "b2" { default="default" }
+variable "b3" { default="default" }
+
 resource "null_resource" "test" {
   triggers = {
     build_number = "${timestamp()}"
   }
+}
+
+output "a1" {
+  value = var.a1
 }
